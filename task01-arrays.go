2e74cf5c9d4d8f4b9228c1a7d617e6748453385a
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-05-09 01:11:16 +0300

    Update task01-arrays.go

--- task01-arrays.go
+++ task01-arrays.go
@@ -5,8 +5,7 @@ func average(input [15]float32) (result float32) {
 var sum float32 = 0
 	for _, number := range input {
 		sum += number
-	}
-	N := float32(len(input))
-	av := sum / N
-	return av	
+	}	
+	result = sum / 15
+	return
 }
