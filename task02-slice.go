ba5434380f594f7ec9b484200103cd481ea786b2
spinkymax <99490816+spinkymax@users.noreply.github.com>
2022-05-09 09:42:22 +0300

    Update task02-slice.go

--- task02-slice.go
+++ task02-slice.go
@@ -3,7 +3,7 @@ package homework
 func reverse(input []int64) (result []int64) {
  	//Place your code here
 	slice := make([]int64, len(input))
-	
+	copy(slice, input)
 	for i := 0; i < len(slice)-1; i++ {
 		for j := 0; j < len(slice)-i-1; j++ {
 			slice[j], slice[j+1] = slice[j+1], slice[j]
